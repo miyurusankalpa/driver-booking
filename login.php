2b37d18140742e1c880b768366fb4761b8af264f
Miyuru Sankalpa <miyurusankalpa@users.noreply.github.com>
2018-08-07 12:29:41 +0530

    added login page
    Signed-off-by: Miyuru Sankalpa <miyurusankalpa@users.noreply.github.com>

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+include 'header.php';
+
+echo '<div class="container">
+	<h1>
+		Login to the System
+	</h1>
+	<br>
+	<form action="Login" method="post">
+		<input type="text" class="form-control"
+			name="username" placeholder="enter a username"> <br>
+		<input type="password" class="form-control"
+			name="password" placeholder="enter a password"> <br> <input
+			type="submit" class="btn btn-primary btn-block" value="Login">
+	</form>
+	
+<br>
+<b>use the username "test" and password "test"" for the first login</b>
+</div>';
+
+include 'footer.php';
+
+?>
