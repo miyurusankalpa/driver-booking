ac3e3569c40724d20ea9a25d5c96e0125375db3c
Miyuru Sankalpa <miyurusankalpa@users.noreply.github.com>
2018-08-28 21:58:39 +0530

    created db.sql and users table

---
+++ DB.sql
@@ -0,0 +1,11 @@
+DROP TABLE `users`;
+
+CREATE TABLE `oop`.`users` (
+  `idusers` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(45) NULL,
+  `password` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL,
+  PRIMARY KEY (`idusers`));
+
+INSERT INTO oop.users (`idusers`,`username`, `password`,`email`) VALUES (null,"test","test","test@test.com");
+INSERT INTO oop.users (`idusers`,`username`, `password`,`email`) VALUES (null,"admin","admin","admin@test.com");
