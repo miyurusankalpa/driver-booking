e739171f58030b4a6abab1dbda268d7302ccfed0
Miyuru Sankalpa <miyurusankalpa@users.noreply.github.com>
2018-08-28 23:13:42 +0530

    fixed db

--- DB.sql
+++ DB.sql
@@ -1,11 +1,11 @@
-DROP TABLE `users`;
+#DROP TABLE `users`;
 
-CREATE TABLE `oop`.`users` (
+CREATE TABLE `users` (
   `idusers` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(45) NULL,
   `password` VARCHAR(45) NULL,
   `email` VARCHAR(45) NULL,
   PRIMARY KEY (`idusers`));
 
-INSERT INTO oop.users (`idusers`,`username`, `password`,`email`) VALUES (null,"test","test","test@test.com");
-INSERT INTO oop.users (`idusers`,`username`, `password`,`email`) VALUES (null,"admin","admin","admin@test.com");
+INSERT INTO users (`idusers`,`username`, `password`,`email`) VALUES (null,"test","test","test@test.com");
+INSERT INTO users (`idusers`,`username`, `password`,`email`) VALUES (null,"admin","admin","admin@test.com");
