a82db2065c703d2ce24ca7484974f72ed00859e7
Miyuru Sankalpa <miyurusankalpa@users.noreply.github.com>
2018-08-31 17:17:04 +0530

    formatted register
    fixed register and added bootstrap

--- register.php
+++ register.php
@@ -1,41 +1,54 @@
-     <div class="header">
-	      <h2>Customer Registration</h2>
-	 </div>
+<div class="jumbotron">
+    <h2>Customer Registration</h2>
+	
+     <form method="post">
+         
+          <div class="form-group row">
+          	<label class="col-sm-2 col-form-label">First Name</label>
+		<div class="col-sm-10">
+          	   <input type="text" class="form-control" name="Firstname">
+         	</div>  
+	   </div>     	
 
-     <form method="post" action="Register.php">
-          <div class="input-group">
-          	   <label>First Name</label>
-          	   <input type="text" name="Firstname">
-          </div>     	
+	   <div class="form-group row">
+          	<label class="col-sm-2 col-form-label">Last Name</label>
+		<div class="col-sm-10">
+          	   <input type="text" class="form-control" name="Lastname">
+         	</div>  
+	   </div>     
 
-          <div class="input-group">
-          	   <label>Last Name</label>
-          	   <input type="text" name="Lastname">
-          </div> 
-       
-          <div class="input-group">
-          	   <label>Username</label>
-          	   <input type="text" name="username">
+	   <div class="form-group row">
+          	<label class="col-sm-2 col-form-label">Username</label>
+		<div class="col-sm-10">
+          	   <input type="text" class="form-control" name="username">
+         	</div>  
+	   </div>  
+
+   	<div class="form-group row">
+          	<label class="col-sm-2 col-form-label">Email</label>
+		<div class="col-sm-10">
+          	   <input type="email" class="form-control" name="Email">
+         	</div>  
+	   </div>  	     
+	     
+  	<div class="form-group row">
+          	<label class="col-sm-2 col-form-label">Password</label>
+		<div class="col-sm-10">
+          	   <input type="password" class="form-control" name="password">
+         	</div>  
+	   </div>  
+	     
+	  <div class="form-group row">
+          	<label class="col-sm-2 col-form-label">Confirm Password</label>
+		<div class="col-sm-10">
+          	   <input type="password" class="form-control" name="Conpassword">
+         	</div>  
+	   </div>  
+
+          <div class="form-group row">
+          	   <button type="submit" name="Register" class="btn btn-success btn-block">Submit</button>
           </div>
-
-          <div class="input-group">
-          	   <label>Email</label>
-          	   <input type="text" name="Email">
-          </div>
-
-          <div class="input-group">
-          	   <label>Password</label>
-          	   <input type="password" name="password">
-          </div>
-
-          <div class="input-group">
-          	   <label>Confirm Password</label>
-          	   <input type="password" name="Conpassword">
-          </div>
-
-          <div class="input-group">
-          	   <button type="submit" name="Register" class="btn">Submit</button>
-          </div>
-		  <p>
-		      If you have alredy registered <a href ="login.php">Sign in</a>
-       </p>
+		  <p class="text-center">
+		      If you have alredy registered <a href="/login.php">Sign in</a>
+       </p> <br> <br>
+ </form></div>
