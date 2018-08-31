dc57b3ab903ca28f11c7c4c596824d5d4a62e507
Madusankawgd <32328026+Madusankawgd@users.noreply.github.com>
2018-08-31 22:04:10 +0530

    Update booking.php

--- booking.php
+++ booking.php
@@ -1,1 +1,35 @@
 
+<div class="jumbotron">
+    <h2>Booking Details</h2>
+	
+     <form method="post">
+         
+		 
+		 <div class="input-group">
+          	   <label>Select the Date:</label>
+          	   <input type="date" name="date">
+			   
+          </div>
+          <div class="input-group">
+          	   <label>Select the Time:</label>
+          	   <input type="time" name="time">
+			   
+          </div>
+
+		  <div class="input-group">
+          	   <label>Enter the pickup location:</label>
+          	   <input type="text" name="pickup">
+			   
+          </div>
+		  
+		  <div class="input-group">
+          	   <label>Enter the Destination:</label>
+          	   <input type="text" name="destination">
+			   
+          </div>
+		   
+          <div class="input-group">
+          	   <button type="submit" name="Register" class="btn">Submit</button>
+          </div>
+          
+ </form></div>
