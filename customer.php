6e9e373d6201d58bba6ca3b03e21ce3cecdf6815
Miyuru Sankalpa <miyurusankalpa@users.noreply.github.com>
2018-08-31 16:55:38 +0530

    added button and table
    add sample code

--- customer.php
+++ customer.php
@@ -1,6 +1,47 @@
 <?php
-//user check
+	include 'header.php';
 
-//book new trips
+echo '<div class="container">
+	<h1>
+		Welcome Customer '.$customer.'
+	</h1>
+	<br>
+	<a href="/new_bookings.php" class="btn btn-info">New Bookings</a>
+	
+<br>
 
-//past booking
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
+</div>';
+
+	include 'footer.php';
+?>
