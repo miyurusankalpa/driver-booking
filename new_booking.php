96b94cd2ca2a6b2410131648ec3e2c01853d6d7d
Miyuru Sankalpa <miyurusankalpa@users.noreply.github.com>
2018-09-01 12:05:22 +0530

    updated new bookings
    renamed the file, added header and footer and formated with bootstrap

---
+++ new_booking.php
@@ -0,0 +1,43 @@
+<?php
+	include 'header.php';
+?>
+<div class="jumbotron">
+    <h2 class="text-center">Booking Details</h2>
+	<hr>
+     <form method="post">	   
+       <div class="form-group row">
+          	<label class="col-sm-3 col-form-label">Select the date</label>
+			<div class="col-sm-9">
+				<input type="date" class="form-control" name="date">
+			</div>  
+	   </div>
+	   
+	   <div class="form-group row">
+          	<label class="col-sm-3 col-form-label">Select the time</label>
+			<div class="col-sm-9">
+				<input type="time" class="form-control" name="time">
+			</div>  
+	   </div>
+	   
+	   <div class="form-group row">
+          	<label class="col-sm-3 col-form-label">Enter the pickup location</label>
+			<div class="col-sm-9">
+				<input type="text" class="form-control" name="pickup">
+			</div>  
+	   </div>
+	   
+	   <div class="form-group row">
+          	<label class="col-sm-3 col-form-label">Enter the destination</label>
+			<div class="col-sm-9">
+				<input type="text" class="form-control" name="destination">
+			</div>  
+	   </div>
+		   
+          <div class="input-group">
+          	   <button type="submit" name="Register" class="btn btn-block btn-success">Submit</button>
+          </div>
+          
+ </form></div>
+<?php
+	include 'footer.php';
+?>
