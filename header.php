fc8153ba8242f47cace87967b8a2c1b0cb55b681
Miyuru Sankalpa <miyurusankalpa@users.noreply.github.com>
2018-09-02 00:48:57 +0530

    nice looking updates
    added title variable, changed the header to navy blue, updated image urls, added a dropdown...
    
    ...reading the fucking code . thanks.

--- header.php
+++ header.php
@@ -1,36 +1,48 @@
+<?php if(empty($title)) $title = "Chauffeur"; ?>
 <!DOCTYPE html>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Home</title>
-<link rel="shortcut icon" href="/favicon.png">
+<title><?php if(isset($title)) echo $title; ?></title>
+<link rel="shortcut icon" href="/images/icon.png">
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <script src="jquery-3.3.1.min.js"></script>
+    <script src="/bootstrap/js/bootstrap.bundle.min.js"></script>
 <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
 <link href="/bootstrap/sticky-footer-navbar.css" rel="stylesheet">
 <link rel="stylesheet" href="/fontawesome/css/fontawesome-all.min.css">
 <style>
-.nav-yellowish
+.nav-navy_blue
 {
-	background-color:#e7da50 !important;
+	background-color:#001f3f !important;
 }
 </style>
 </head>
 <body>
 	<script type="text/javascript">var j = jQuery.noConflict();</script>
  <header>
-	<nav class="navbar navbar-expand-lg navbar-dark nav-yellowish"> 
+	<nav class="navbar navbar-expand-lg navbar-dark nav-navy_blue"> 
 	<a
 		class="navbar-brand" href="#">
-		<img src="/favicon.png" height="20px" /> the chauffeur</a>
+		<!--<img src="/images/icon.png" height="20px" />--> the chauffeur</a>
 		<ul class="navbar-nav mr-auto">
 		<li class="nav-item"><a class="nav-link text-white" href="/index.php"><i class="fa fa-home"></i> Home</a>
 		</li>
 	</ul>
 	<ul class="navbar-nav">
 		<li class="nav-item">
-			<li class="nav-item"><a class="nav-link text-white" href="/login.php"><i class="fa fa-home"></i> Login</a>
+		<?php if(isset($_COOKIE["user"])) 
+					echo '<li class="nav-item dropdown" role="button" data-toggle="dropdown">
+						<a class="nav-link text-white dropdown-toggle" href="#"><i class="fa fa-user"></i> Hello '.$_COOKIE["user"].'</a>
+						<div class="dropdown-menu dropdown-menu-right">
+						  <span class="dropdown-item">  <a href="/test">Regular link</a></span>
+						  <div class="dropdown-divider"></div>
+						  <a class="dropdown-item" href="/logout.php">logout</a>
+						</div>'; 
+				else
+					echo '<li class="nav-item"><a class="nav-link text-white" href="/login.php"><i class="fa fa-user"></i> Login</a>'; 
+		?>
  		</li>
 	</ul>
 	</nav>
