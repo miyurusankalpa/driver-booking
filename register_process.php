1f51f370f2258c50c5cf8ca69c04dbbdcdca5695
Miyuru Sankalpa <miyurusankalpa@users.noreply.github.com>
2018-09-02 00:42:29 +0530

    fix register_process
    update to mysqli_real_escape, added json header and fixed the json output
    
    Co-Authored-By: pinsara <pinsara@users.noreply.github.com>

--- register_process.php
+++ register_process.php
@@ -1,66 +1,72 @@
-<?php  
+<?php  //inception
+header('Content-Type: application/json');
+
 	include_once 'mysqli.php';
-	$error = array();
+
+	$error = array(); 	$array = array();
+	
 	// connecting to database
-	$sql =new mysqli($server, $user, $pass, $db)
+	$sql = new mysqli($server, $user, $pass, $db);
 
 	if (isset($_POST['register'])) {
 
-		$Firstname = mysql_real_escape_string($_POST['Firstname']);
-		$Lastname = mysql_real_escape_string($_POST['Lastname']);
-		$username = mysql_real_escape_string($_POST['username']);
-		$Email = mysql_real_escape_string($_POST['Email']);
-		$password = mysql_real_escape_string($_POST['password']);
-		$Conpassword = mysql_real_escape_string($_POST['Conpassword']);
-		$mobileNo = mysql_real_escape_string($_POST['mobileNo']);
+		$Firstname = mysqli_real_escape_string($sql, $_POST['Firstname']);
+		$Lastname = mysqli_real_escape_string($sql, $_POST['Lastname']);
+		$username = mysqli_real_escape_string($sql, $_POST['username']);
+		$email = mysqli_real_escape_string($sql, $_POST['Email']);
+		$password = mysqli_real_escape_string($sql, $_POST['password']);
+		$conpassword = mysqli_real_escape_string($sql, $_POST['Conpassword']);
+		$mobileNo = mysqli_real_escape_string($sql, $_POST['mobileNo']);
 
-		if (empty($Firstname)) {
-			
+		if(empty($Firstname)) {
 				array_push($error, "Firstname is required");
 		}
 
-		if (empty($Lastname)) {
-
+		if(empty($Lastname)) {
 				array_push($error, "Lastname is required");
 		}
-		if (empty($username) ) {
-				
+		
+		if(empty($username) ) {
 				array_push($error, "Username is required"); //error msg username
 		}
-		if (empty($Email)) {
-				
+		
+		if(empty($email)) {
 				array_push($error, "Email is required");  //error msg password
 		}
-		if (empty($password)) {
-				
-				array_push($error, "password is required");
 		
-		}if (empty($Conpassword)) {
-			
-				array_push($error, "confirm password is required");
+		if(empty($password)) {
+				array_push($error, "Password is required");
+		}
+		
+		if(empty($Conpassword)) {
+				array_push($error, "Confirm password is required");
 		}
 
-		if ($password != Conpassword) {
-			
-				array_push($error, "passwords do not match.Try again");
+		if ($password != $conpassword) {
+				array_push($error, "Confim passwords do not match. Try again");
 		}
 
 		if (count($error)== 0) {
-			
-				//$encryptpass = md5($password);//encryption
-				$query = "INSERT INTO user (Firstname, Lastname, username, email, password, mobileNo) VALUES ('$Firstname', '$Lastname', '$username', '$Email', '$password', '$mobileNo')";
+				$encryptpass = md5($password); //encryption
+				$query = "INSERT INTO user (firstname, lastname, username, email, password, mobileno) VALUES ('$Firstname', '$Lastname', '$username', '$Email', '$encryptpass', '$mobileNo')";
 				
 				$x = mysqli_query($sql, $query);
 			
 			if($x == 1){
-				
-				die("Successful");
-			}
+				$array["result"] = "success";
+				$array["message"] = "Registration Successful";
+			} else array_push($error, "Error adding date to the database. Try again");
+		} else {
+			$array["result"] = "error";
+			$array["message"] = $error;
 		}
-		
-
-		$json = json_encode($error);
-
-		echo $json;
+	} else {
+				$array["result"] = "error";
+				$array["message"] = "Empty data";
 	}
 
+	$json = json_encode($array);
+
+	echo $json;
+
+?>
