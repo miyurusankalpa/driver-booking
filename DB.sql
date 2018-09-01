9130c842eaa35c8df1342ed664a97b7841aa8b8c
Miyuru Sankalpa <miyurusankalpa@users.noreply.github.com>
2018-09-02 00:36:11 +0530

    update users table
    added more colomns to the table and updated the test user
    
    Co-Authored-By: Hasiya <hasithsen@users.noreply.github.com>

--- DB.sql
+++ DB.sql
@@ -1,11 +1,13 @@
-#DROP TABLE `users`;
+DROP TABLE `users`;
 
 CREATE TABLE `users` (
   `idusers` INT NOT NULL AUTO_INCREMENT,
+  `firstname` VARCHAR(45) NULL,
+  `lastname` VARCHAR(45) NULL,
   `username` VARCHAR(45) NULL,
   `password` VARCHAR(45) NULL,
+  `mobileno` INT(10) NULL,
   `email` VARCHAR(45) NULL,
   PRIMARY KEY (`idusers`));
 
-INSERT INTO users (`idusers`,`username`, `password`,`email`) VALUES (null,"test","test","test@test.com");
-INSERT INTO users (`idusers`,`username`, `password`,`email`) VALUES (null,"admin","admin","admin@test.com");
+INSERT INTO `users` (`idusers`, `firstname`, `lastname`, `username`, `password`, `mobileno`, `email`) VALUES (NULL, 'fname', 'lname', 'test', '098f6bcd4621d373cade4e832627b4f6', '00000000', 'test@example.com'); 
