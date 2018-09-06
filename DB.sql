99605ff230acc2c176bff3c5d55fe07bfc67cc0c
hasithsen <it17142038@my.sliit.lk>
2018-09-06 15:30:53 +0530

    Added SQL query for 'booking' table creation

--- DB.sql
+++ DB.sql
@@ -12,3 +12,12 @@ CREATE TABLE `users` (
   PRIMARY KEY (`idusers`));
 
 INSERT INTO `users` (`idusers`, `firstname`, `lastname`, `username`, `password`, `mobileno`, `email`) VALUES (NULL, 'fname', 'lname', 'test', '098f6bcd4621d373cade4e832627b4f6', '00000000', 'test@example.com'); 
+
+CREATE TABLE `booking` (
+  `user_id` INT NOT NULL AUTO_INCREMENT,
+  `date` VARCHAR(45) NULL,
+  `time` VARCHAR(45) NULL,
+  `pickup` VARCHAR(45) NULL,
+  `destination` VARCHAR(45) NULL,
+  PRIMARY KEY (`user_id`)
+);
