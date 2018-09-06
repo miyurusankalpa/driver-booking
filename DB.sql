6acdc8eda65940eaa55e85c95c55f6dd3da46f62
hasithsen <it17142038@my.sliit.lk>
2018-09-06 15:19:32 +0530

    Added user group colum

--- DB.sql
+++ DB.sql
@@ -8,6 +8,7 @@ CREATE TABLE `users` (
   `password` VARCHAR(45) NULL,
   `mobileno` INT(10) NULL,
   `email` VARCHAR(45) NULL,
+  `group` VARCHAR(45) NULL,
   PRIMARY KEY (`idusers`));
 
 INSERT INTO `users` (`idusers`, `firstname`, `lastname`, `username`, `password`, `mobileno`, `email`) VALUES (NULL, 'fname', 'lname', 'test', '098f6bcd4621d373cade4e832627b4f6', '00000000', 'test@example.com'); 
