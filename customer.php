7d4c7ff7edebd2b9c8098581b02a69a3c2e0245f
Miyuru Sankalpa <miyurusankalpa@users.noreply.github.com>
2018-09-08 23:44:10 +0530

    booking table
    connected booking table from the backend

--- customer.php
+++ customer.php
@@ -8,37 +8,40 @@ echo '<div class="container">
 	<br>
 	<a href="/new_booking.php" class="btn btn-info">New Bookings</a>
 	
-<br>
+<br><br>
 
 <table class="table">
   <thead>
     <tr>
       <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">Date</th>
+      <th scope="col">Pickup</th>
+      <th scope="col">Destination</th>
+	  <th scope="col">Status</th>
     </tr>
   </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
+  <tbody>';
+  
+  include_once 'mysqli.php';
+  if(!isset($mysqli)) $mysqli = new mysqli($server, $user, $pass, $db);
+	
+  $query = "SELECT * FROM `booking` WHERE `user_id` = ".$_COOKIE['user'];
+				
+	$result = mysqli_query($mysqli, $query);
+	
+	 while ($row=mysqli_fetch_assoc($result))
+    {
+	echo '<tr>
+      <th scope="row">'.$row['booking_id'].'</th>
+      <td>'.$row['date'].'</td>
+      <td>'.$row['pickup'].'</td>
+      <td>'.$row['destination'].'</td>
+	  <td>'.$row['status'].'</td>
+	</tr>';
+	
+	}
+
+echo '</tbody>
 </table>
 
 </div>';
